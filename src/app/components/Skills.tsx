e5fa1eba744311a0a9763d833467f9d5d22ff0a9
matt <matthewgordon64@gmail.com>
2024-04-17 19:41:46 -0400

    changes to skills & projects

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,15 +4,97 @@ const Skills = () => {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8">Skills</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-          <div className="flex items-center">
-            <img
-              src="/skill1.png"
-              alt="Icon representing proficiency in HTML"
-              className="w-12 h-12 mr-4"
-            />
-            <p className="text-lg font-bold">HTML</p>
+          <div className="flex flex-wrap">
+            <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+              <div className="flex items-center"></div>
+              <img
+                src="/cloudflare-logo.png"
+                alt="Cloudflare Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">
+                Pages | Workers | R2 | KV | D1 | Edge Runtime{" "}
+              </p>
+            </div>
           </div>
-          {/* Add more skill items */}
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/huggingface-logo.png"
+                alt="HuggingFace Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">
+                Transformers | LLMs | Video | ML Development
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/openai-logo.png"
+                alt="OpenAI Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">
+                OpenAI GPT4 | DALLE-3 | Function Calling | TTS | Whisper
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/anthropic-logo.png"
+                alt="Anthropic Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">Anthropic | Claude 3</p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/nextjs-logo.png"
+                alt="NextJS Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">
+                NextJS Full Stack Framework (Edge Enabled)
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/expo-logo.png"
+                alt="Cloudflare Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">
+                Expo React Native Mobile App Development
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/python-logo.png"
+                alt="Python Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">Python</p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/4 mb-8">
+            <div className="flex items-center">
+              <img
+                src="/typescript-logo.png"
+                alt="Typescript Logo"
+                className="w-12 h-12 mr-4"
+              />
+              <p className="text-lg font-bold">Typescript</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/app/config.ts
+++ src/app/config.ts
@@ -7,22 +7,6 @@ export const config = {
             description: "Matt's KPI Dashboard",
         },
         {
-            title: "Matt's AI Agents",
-            url: "https://agents.matthew.tech",
-            description: "Matt's AI Agents | Special News Agent | MOE Agent Framework | Deploy Agent",
-        },
-        {
-            title: "Matt's AI Deploy Agent",
-            url: "https://deploy.matthew.tech",
-            description: "Deploy with Matt's AI Agents",
-        },
-        {
-            title: "Matt's AI News Agent",
-            url: "https://news.matthew.tech",
-            logo: "/news-agent-hero.png",
-            description: "AI News | News Aggregator | News API",
-        },
-        {
             title: "Shortify",
             logo: "/shortify-logo.png",
             banner: "/shortify-banner.gif",
@@ -33,13 +17,7 @@ export const config = {
             url: "https://tryomni.lol",
             logo: "/tryomni-dash.png",
             banner: "/tryomni-banner.png",
-            description: "Creator & Brands Collide | TryOmni",
-        },
-        {
-            title: "Treats Digital",
-            url: "https://treats.digital",
-            logo: "/treats-pay.png",
-            description: "Creator Video Marketplace | Treats Digital",
+            description: "Ecommerce Influencer Marketing + Analytics Dashboard | TryOmni",
         },
         {
             title: "MyStory",
@@ -51,19 +29,37 @@ export const config = {
             title: "KnowledgeGate",
             url: "https://knowledgegate.lol",
             logo: "/knowledge-gate-hero.png",
-            banner: "adam-bragg.png",
+            // banner: "adam-bragg.png",
             description: "KnowledgeGate | Partnership with Adam Bragg | Knowledge Gate App",
         },
         {
-            title: "Accenture Job (secret projects)",
+            title: "Accenture Job",
             url: "https://accenture.com",
             logo: "/sap-logo.webp",
             banner: "/accenture-logo.png",
             description: "I do SAP & GenAI | Accenture Job",
         },
         {
-            title: "NL Leads System Site (TBD)",
-            description: "NL Leads System Site (WIP)",
+            title: "*TBD* Treats Digital",
+            url: "https://treats.digital",
+            logo: "/treats-pay.png",
+            description: "Creator Video Marketplace | Treats Digital",
+        },
+        {
+            title: "*TBD* Matt's AI Agents",
+            url: "https://agents.matthew.tech",
+            description: "Matt's AI Agents | Special News Agent | MOE Agent Framework | Deploy Agent",
+        },
+        {
+            title: "*TBD* Matt's AI Deploy Agent",
+            url: "https://deploy.matthew.tech",
+            description: "Deploy with Matt's AI Agents",
+        },
+        {
+            title: "*TBD* Matt's AI News Agent",
+            url: "https://news.matthew.tech",
+            logo: "/news-agent-hero.png",
+            description: "AI News | News Aggregator | News API",
         }
     ]
 }
